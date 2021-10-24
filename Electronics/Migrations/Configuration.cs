38ffc7681eafffe93fb21587e4c728e16fed5b9b
Bartek <bart_tux@bartek-komp.localdomain>
2021-10-24 22:40:13 +0200

    revision

---
+++ Electronics/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Electronics.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Electronics.Models.CalculatorContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Electronics.Models.CalculatorContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
--- Electronics/Models/CalculatorContext.cs
+++ Electronics/Models/CalculatorContext.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace Electronics.Models
 {
-    public class CalculatorContext
+    public class CalculatorContext : DbContext
     {
+        public CalculatorContext() : base("name=CalculatorDB") { }
+
+        public DbSet<SearchHistory> SearchHistory { get; set; }
+        public DbSet<Module> Module { get; set; }
+        public DbSet<City> City { get; set; }
     }
 }
